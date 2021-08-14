15d78d57097bde8d72df19fdbcfc255292d97e77
SrLabres <josue.labres.cruz@gmail.com>
2021-08-14 11:02:23 -0300

    Listando os posts do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../services/prismic';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -24,41 +25,22 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
- export default function Home() {
-   
+ export default function Home({ postsPagination }: HomeProps) {
+  
   return (
     <>
       <main className={commonStyles.container}>
           <ul className={styles.postsList}>
-            <li>
-              <h2>Como utilizar Hooks</h2>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <span><FiCalendar /> 15 Mar 2021</span><span><FiUser /> Joseph Oliveira</span>
-            </li>
+            { postsPagination.results.map((post) => (
+              <li key={post.uid}>
+                <h2>{ post.data.title }</h2>
+                <p>{ post.data.subtitle}</p>
+                <span><FiCalendar /> { post.first_publication_date }</span><span><FiUser /> { post.data.author }</span>
+              </li>
+            ))}
+            
 
-            <li>
-              <h2>Como utilizar Hooks</h2>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <span><FiCalendar /> 15 Mar 2021</span><span><FiUser /> Joseph Oliveira</span>
-            </li>
-
-            <li>
-              <h2>Como utilizar Hooks</h2>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <span><FiCalendar /> 15 Mar 2021</span><span><FiUser /> Joseph Oliveira</span>
-            </li>
-
-            <li>
-              <h2>Como utilizar Hooks</h2>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <span><FiCalendar /> 15 Mar 2021</span><span><FiUser /> Joseph Oliveira</span>
-            </li>
-
-            <li>
-              <h2>Como utilizar Hooks</h2>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <span><FiCalendar /> 15 Mar 2021</span><span><FiUser /> Joseph Oliveira</span>
-            </li>
+            
 
             <a href="#">Carregar mais posts</a>
           </ul>
@@ -68,9 +50,41 @@ interface HomeProps {
 
  }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+ export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+    const postsResponse = await prismic.query([
+      Prismic.predicates.at('document.type', 'posts')
+    ],
+      {
+        fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+        pageSize: 5,
+      }
+    );
+      
+    const results = postsResponse.results.map(post => {
+      return {
+        slug: post.uid,
+        first_publication_date: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    })
 
-//   // TODO
-// };
+    const next_page = postsResponse.next_page;
+    return {
+      props: {
+        postsPagination: {
+          next_page,
+          results
+        }
+      }
+    }
+
+ };
